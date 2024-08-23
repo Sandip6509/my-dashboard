b07558719eac06a67932921af6d3b6e0a2754b35
Sandip6509 <sandippatel3101@gmail.com>
2024-08-23 21:19:54 +0530

    dashboard setup

---
+++ src/app/(dashboard)/list/lessons/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function LessionListPage() {
+    return (
+        <div>LessionListPage</div>
+    )
+}
---
+++ src/app/sign-in/page.tsx
@@ -0,0 +1,5 @@
+export default function Loginpage() {
+  return (
+    <div className=''>Loginpage</div>
+  )
+}
